dc5e92f85ba227979aa18e197af7188dc5716c0b
22bcs16819 <22bcs16819@cuchd.in>
2026-08-08 11:28:17 +0530

    First Commit

---
+++ src/main/java/com/example/github_cicd/TestApi.java
@@ -0,0 +1,16 @@
+package com.example.github_cicd;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestApi {
+    
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String test() {
+        return "Hello World";
+    }
+
+}
